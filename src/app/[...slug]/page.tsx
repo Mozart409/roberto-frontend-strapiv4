6dfd257aafe1cd9c2fcde4859d84bc69a93b9704
Amadeus <38767929+Mozart409@users.noreply.github.com>
2025-03-09 23:13:21 +0100

    fix: :bug: fixed bugs

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -3,6 +3,7 @@ import { getPageBySlug } from "@/app/utils/get-page-by-slug";
 import { sectionRenderer } from "@/app/utils/section-renderer";
 import type { Metadata } from "next";
 
+// Define props type
 type Props = {
   params: {
     lang: string;
@@ -10,32 +11,40 @@ type Props = {
   };
 };
 
+// Generate metadata for SEO
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const page: any = await getPageBySlug(params.slug, params.lang);
-  if (page.data[0].length === 0 && page.data[0].attributes.seo === undefined) {
+
+  // Debug log (consider removing in production)
+  console.debug("page data length", page.data.length);
+
+  // Check if page data is empty or lacks SEO attributes
+  if (page.data.length === 0 || !page.data[0]?.attributes?.seo) {
     return FALLBACK_SEO;
   }
 
   const metadata = page.data[0].attributes.seo;
-
   return {
     title: metadata.metaTitle,
     description: metadata.metaDescription,
   };
 }
 
+// Page component
 export default async function PageRoute({ params }: Props) {
   const page: any = await getPageBySlug(params.slug, params.lang);
-  // if (page.data[0].length === 0) return null
-  if (page.data[0].length === 0) {
+
+  // Return 404 if no page data is found
+  if (page.data.length === 0) {
     return (
       <>
         <h1 className="text-3xl font-bold">404 Content not found!</h1>
       </>
     );
   }
+
   const contentSections = page.data[0].attributes.contentSections;
   return contentSections.map((section: any, index: number) =>
-    sectionRenderer(section, index),
+    sectionRenderer(section, index)
   );
 }
