4e3ae5954c8fa3e5142f3f3de0840b9de77ab936
Amadeus <38767929+Mozart409@users.noreply.github.com>
2025-03-23 01:09:56 +0100

    feat: :fire: remove unused dependencies and middleware logic for code cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,4 @@
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-import { i18n } from "../i18n-config";
-
-import { match as matchLocale } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-
-function getLocale(request: NextRequest): string | undefined {
-  // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {};
-  // biome-ignore lint/complexity/noForEach: <explanation>
-  // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  // Use negotiator and intl-localematcher to get best locale
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales;
-
-  // console.debug("languages", languages);
-  return matchLocale(languages, locales, i18n.defaultLocale);
-}
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
